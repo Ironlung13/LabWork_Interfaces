2ca3db8009dd0455d00279da225a03b4c8999deb
ironlung13 <ironlung13@mail.ru>
2021-03-10 18:07:33 +0300

    Implement added class, change ProgramHelper to inherit from new ProgramConverter class

---
+++ Classes/ProgramConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork_Interfaces.Classes
+{
+    public class ProgramConverter : Interfaces.IConvertible
+    {
+        public string ConvertToCSharp(string input)
+        {
+            //Convert to c#
+            return "Success";
+        }
+
+        public string ConvertToVB(string input)
+        {
+            //Convert to VB
+            return "Success";
+        }
+    }
+}
--- Classes/ProgramHelper.cs
+++ Classes/ProgramHelper.cs
@@ -4,24 +4,12 @@ using System.Text;
 
 namespace LabWork_Interfaces.Classes
 {
-    public class ProgramHelper : Interfaces.IConvertible, Interfaces.ICodeChecker
+    public class ProgramHelper : ProgramConverter, Interfaces.ICodeChecker
     {
         public bool CheckCodeSyntax(string input, string language)
         {
             //Check code;
             return true;
-        }
-
-        public string ConvertToCSharp(string input)
-        {
-            //Convert code to C#
-            return "Success";
-        }
-
-        public string ConvertToVB(string input)
-        {
-            //Convert code to VB
-            return "Success";
         }
     }
 }
